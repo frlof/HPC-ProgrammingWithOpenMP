1a91ea4bcfee407419f1fffb124ceb482436ef70
RoboticNoob <victor.carle@gmail.com>
2019-04-16 10:45:47 +0200

    hej

--- OpenMP/exercise_2/src/sum.c
+++ OpenMP/exercise_2/src/sum.c
@@ -31,17 +31,47 @@ void omp_critical_sum(double *sum_ret)
 
 void omp_atomic_sum(double *sum_ret)
 {
-
+    omp_set_num_threads(32);
+    int sum = 0;
+    int iterations = sizeof(sum_ret) / sizeof(sum_ret[0]);
+    #pragma omp parallel
+    {
+        #pragma omp for
+        for(int i = 0; i < iterations; i++){
+            #pragma omp atomic
+            sum += sum_ret[i];
+        }
+    }
 }
 
 void omp_local_sum(double *sum_ret)
 {
-
+    omp_set_num_threads(32);
+    int sum[32];
+    int iterations = sizeof(sum_ret) / sizeof(sum_ret[0]);
+    #pragma omp parallel
+    {
+        int id = omp_get_thread_num;
+        #pragma omp for
+        for(int i = 0; i < iterations; i++){
+            sum[id] += sum_ret[i];
+        }
+    }
 }
 
 void omp_padded_sum(double *sum_ret)
 {
-
+    omp_set_num_threads(32);
+    int sum[32][8];
+    int iterations = sizeof(sum_ret) / sizeof(sum_ret[0]);
+    #pragma omp parallel
+    {
+        int id = omp_get_thread_num;
+        #pragma omp for
+        for(int i = 0; i < iterations; i++){
+            sum[id][0] += sum_ret[i];
+        }
+    }
 }
 
 void omp_private_sum(double *sum_ret)
