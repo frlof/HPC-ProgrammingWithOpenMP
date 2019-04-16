03ab6b301c65b96f0d42cf7036e9fc59789ff429
RoboticNoob <victor.carle@gmail.com>
2019-04-16 13:03:40 +0200

    hej

--- OpenMP/exercise_2/src/sum.c
+++ OpenMP/exercise_2/src/sum.c
@@ -2,7 +2,7 @@
 
 void omp_sum(double *sum_ret)
 {
-    omp_set_num_threads(32);
+    //omp_set_num_threads(32);
     int sum = 0;
     int iterations = sizeof(sum_ret) / sizeof(sum_ret[0]);
     #pragma omp parallel
