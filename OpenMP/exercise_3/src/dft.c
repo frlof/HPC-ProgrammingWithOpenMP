5d3eec39a88ab7358c0b2881469aa1deafcfb290
Fredrik <frlof@kth.se>
2019-05-02 15:41:40 +0200

    added serial version of program in exercise 3

--- OpenMP/exercise_3/src/dft.c
+++ OpenMP/exercise_3/src/dft.c
@@ -2,7 +2,7 @@
 
 // DFT/IDFT routine
 // idft: 1 direct DFT, -1 inverse IDFT (Inverse DFT)
-int DFT(int idft, double* xr, double* xi, double* Xr_o, double* Xi_o, int N)
+int aDFT(int idft, double* xr, double* xi, double* Xr_o, double* Xi_o, int N)
 {
 	#pragma omp parallel
 	{
@@ -14,6 +14,27 @@ int DFT(int idft, double* xr, double* xi, double* Xr_o, double* Xi_o, int N)
 			// Imaginary part of X[k]
 			Xi_o[k / N] += -idft*xr[n] * sin(n * (k / N) * PI2 / N) + xi[n] * cos(n * (k / N) * PI2 / N);  
 		} 
+	}
+		
+	// normalize if you are doing IDFT
+	if (idft == -1){
+		for (int n = 0; n < N; n++) {
+			Xr_o[n] /= N;
+			Xi_o[n] /= N;
+		}
+	}
+	return 1;
+}
+
+int DFT(int idft, double* xr, double* xi, double* Xr_o, double* Xi_o, int N)
+{
+	for (int k = 0 ; k < N; k++) {
+		for (int n = 0; n < N; n++) {
+			// Real part of X[k]
+			Xr_o[k] += xr[n] * cos(n * k * PI2 / N) + idft * xi[n] * sin(n * k * PI2 / N);
+			// Imaginary part of X[k]
+			Xi_o[k] += -idft * xr[n] * sin(n * k * PI2 / N) + xi[n] * cos(n * k * PI2 / N);
+		}
 	}
 		
 	// normalize if you are doing IDFT
