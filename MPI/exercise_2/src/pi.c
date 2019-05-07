8aed6d8f71a61573f4e7f4743d1922cedba628ce
Fredrik <frlof@kth.se>
2019-05-07 14:23:13 +0200

    parallel pi (no no blocking)

--- MPI/exercise_2/src/pi.c
+++ MPI/exercise_2/src/pi.c
@@ -1,4 +1,5 @@
 #include "pi.h"
+#include <math.h>
 
 void init_pi(int set_seed, char *outfile)
 {
@@ -23,4 +24,46 @@ void cleanup_pi()
 
 void compute_pi(int flip, int *local_count, double *answer)
 {
+	int world_rank;
+	int num_ranks;
+	MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+	MPI_Comm_size(MPI_COMM_WORLD, &num_ranks);
+
+	double size = 1.0;
+	double radius = 1.0;
+
+	for(int i = 0; i < flip / (double)num_ranks; i++){
+		double newCoordX = (double)rand();
+		double newCoordY = (double)rand();
+
+		double distance = sqrt(newCoordX*newCoordX+newCoordY*newCoordY);
+
+		if(distance <= radius){
+			*local_count++;
+		}
+	}
+
+	
+
+	int world_rank;
+	
+	if (world_rank == 0) {
+		int count = *local_count;
+		int temp;
+		for(int i = 1; i < num_ranks; i++){
+			
+    		MPI_Recv(&temp, 1, MPI_INT, i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+			count += temp;
+		}
+		
+		double P = success / total;
+		double pi = 4 * P;
+
+		*answer = pi;
+
+		printf("pi: %f\n", pi);
+	}else{
+		MPI_Send(local_count, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
+	}
+	
 }
