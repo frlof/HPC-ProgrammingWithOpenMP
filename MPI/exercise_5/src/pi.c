32c98a82f9b908b86e4bb2e4710495c712d16ee0
RoboticNoob <victor.carle@gmail.com>
2019-05-13 11:10:05 +0200

    apa

--- MPI/exercise_5/src/pi.c
+++ MPI/exercise_5/src/pi.c
@@ -23,4 +23,31 @@ void cleanup_pi()
 
 void compute_pi(int flip, int *local_count, double *answer)
 {
+	int world_rank;
+	int num_ranks;
+	MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+	MPI_Comm_size(MPI_COMM_WORLD, &num_ranks);
+	srand(world_rank * seed);
+	int i;
+	for(i = 0; i < (int)((double)flip / (double)num_ranks); i++){
+		double x = (double)rand() / (double)RAND_MAX;
+		double y = (double)rand() / (double)RAND_MAX;
+		double distance = sqrt((x*x) + (y*y));
+		if(distance <= 1.0){
+			(*local_count)++;
+		}
+	}
+	int count;
+	MPI_Reduce(local_count, &count, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
+
+	if (world_rank == 0) {
+		
+		double P = (double)count / (double)flip;
+		double pi = 4 * P;
+
+		*answer = pi;
+	}
+	MPI_File fh = "results.txt";
+	MPI_File_open(MPI_COMM_WORLD, "results.txt", MPI_MODE_WRONLY, MPI_INFO_NULL, &fh);
+	MPI_File_write_all("results.txt", &local_count, 1, MPI_INT, MPI_STATUS_IGNORE);
 }
