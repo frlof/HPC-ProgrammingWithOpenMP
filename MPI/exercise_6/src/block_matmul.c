9d5926b0256e7d5074633ce82caa250ad3450e4c
RoboticNoob <victor.carle@gmail.com>
2019-05-15 00:22:12 +0200

    hej

--- MPI/exercise_6/src/block_matmul.c
+++ MPI/exercise_6/src/block_matmul.c
@@ -182,7 +182,7 @@ void compute_fox()
 		int inRow;
 
 		MPI_Comm_rank(config.row_comm, &rowID);
-		MPI_Comm_size(config.row_comm, &inRow);
+		MPI_Comm_rank(config.col_comm, &inRow);
 		printf("localSize: %d\n", config.local_size);
 		/*if(config.world_rank == 0){
 			printf("[%d]   ID:%d   N:%d\n", config.world_rank, rowID, inRow);
