f85866c8e08d8672343181929271d0deff0ca583
RoboticNoob <victor.carle@gmail.com>
2019-05-14 16:46:27 +0200

    apa

--- MPI/exercise_6/src/block_matmul.c
+++ MPI/exercise_6/src/block_matmul.c
@@ -45,11 +45,11 @@ void init_matmul(char *A_file, char *B_file, char *outfile)
 	config.dim[0] = 2;
 	config.dim[1] = 2;
 	MPI_Cart_create(MPI_COMM_WORLD, 2, config.dim, wrap, 1, &config.grid_comm);
-	if(config.world_rank == 0){
-		int coord[2];
-		MPI_Cart_coords(config.grid_comm, config.world_rank, 2, coord);
-		printf("%d\n", coord[0]);
-	}
+	//if(config.world_rank == 0){
+	int coord[2];
+	MPI_Cart_coords(config.grid_comm, config.world_rank, 2, coord);
+	printf("%d\n", coord[0]);
+	//}
 }
 
 void init_matmuls(char *A_file, char *B_file, char *outfile)
